5a64068f260889fc46f82f2f4d6991720e5abe5b
Omar Soto <ojsg140789@gmail.com>
2024-11-01 22:29:12 -0600

    refactor(layout): implement RouteTitleService for dynamic page title management

--- src/app/features/layout/layout.component.ts
+++ src/app/features/layout/layout.component.ts
@@ -4,12 +4,15 @@ import { AplazoButtonComponent } from '@apz/shared-ui/button';
 import { AplazoDashboardComponents } from '@apz/shared-ui/dashboard';
 import { AplazoSidenavLinkComponent } from '../../../../projects/shared-ui/sidenav/src';
 import { ROUTE_CONFIG } from '../../core/infra/config/routes.config';
+import { CommonModule } from '@angular/common';
+import { RouteTitleService } from '../../core/services/route-title.service';
 
 @Component({
   standalone: true,
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   imports: [
+    CommonModule,
     AplazoDashboardComponents,
     AplazoButtonComponent,
     AplazoSidenavLinkComponent,
@@ -21,6 +24,8 @@ export class LayoutComponent {
   readonly #router = inject(Router);
 
   readonly appRoutes = ROUTE_CONFIG;
+  
+  public pageTitle$ = inject(RouteTitleService).title$;
 
   clickLogo(): void {
     this.#router.navigate([ROUTE_CONFIG.home]);
