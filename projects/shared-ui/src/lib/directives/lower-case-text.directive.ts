99ae60aa796df37815b8c06135574a116891fc47
Omar Soto <ojsg140789@gmail.com>
2024-11-01 20:35:54 -0600

    feat: complete lowerCaseDirective to prevent UpperCase

--- projects/shared-ui/src/lib/directives/lower-case-text.directive.ts
+++ projects/shared-ui/src/lib/directives/lower-case-text.directive.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @angular-eslint/no-host-metadata-property */
 import { Directive, ElementRef, inject } from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
 
 @Directive({
   standalone: true,
@@ -17,8 +18,19 @@ export class AplazoLowercaseDirective {
   });
 
   sanitizeValue(event: InputEvent): void {
+    const input = this.#elementRef.nativeElement;
+    
     // TODO: sanitize the value to lowercase
+    const sanitizedValue = input.value.toLowerCase();
+    input.value = sanitizedValue;
+  
     // TODO: propagate the value to the NgControl
+    const control = this.#ngControl?.control;
+    if (control) {
+      control.setValue(sanitizedValue);
+    }
+  
     // TODO: preserve the cursor position
+    input.setSelectionRange(input.selectionStart, input.selectionEnd);
   }
 }
