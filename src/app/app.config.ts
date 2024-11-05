3ced5d527b497dcbfc7f29eea29cf6c0dbb66903
Omar Soto <ojsg140789@gmail.com>
2024-11-04 20:59:19 -0600

    chore: install and configure ngx-toastr for error notifications

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { AuthInterceptor } from './core/services/interceptors/auth.interceptor';
 
@@ -8,6 +10,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      ToastrModule.forRoot({
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    })),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ]
 };
